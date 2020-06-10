43740ae12f16947037ae09ea1b028e5b81c85d49
couscousdude <youwenw@gmail.com>
2020-06-09 21:49:53 -0700

    Updated index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 // config.json contains general info like token and prefix
-const config = require('./config.json');
+const { prefix, token } = require('./config.json');
 
 // RegExp for detecting variants of hello
 const helloRegex = /\s*h\s*e+\s*l+\s*[o|0]+\s*/i;
@@ -9,9 +9,9 @@ const helloRegex = /\s*h\s*e+\s*l+\s*[o|0]+\s*/i;
 // say hello function
 // 1 in 100 chance to actually be nice
 function sayHello() {
-    const randomNumber = () => Math.floor(Math.random() * 100) === 0;
+    const randomNumber = () => Math.floor(Math.random() * 100);
     console.log(randomNumber);
-    if (randomNumber) {
+    if (randomNumber === 0) {
         return 'whats up';
     };
     return 'go away';
@@ -21,17 +21,13 @@ client.on('message', message => {
     // P bof message sender
 	if (message.content === 'p') {
         message.channel.send('bof');
-    };
-    // stink stonk message sender
-    if (message.content === 'stink') {
+    } else if (message.content === 'stink') {
         message.channel.send('stonk');
-    };
-    // bof bot has no manners
-    if (helloRegex.test(message.content)) {
+    } else if (helloRegex.test(message.content)) {
         message.channel.send(sayHello());
     };
     console.log(message.content);
 });
 
 // login the bot
-client.login(config.token);
+client.login(token);
